39a7db21e3ce9d10d2d775575c981ee34f3e4b05
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-06-06 09:58:38 -0500

    Cambios en el main

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
     Video* video = new Movie(36, "2h", "Comedy", 5);
     Serie* serie = new Serie(2, "1h", "Action", "1", "1", 1, video);
-    //serie->print();
-    video ->print();
+    serie->print();
+    //video ->print();
     return 0;
 }
